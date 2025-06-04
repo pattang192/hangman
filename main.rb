9e7f530cc6d20664bb196f9c23a36d6a3f2b4716
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-06-04 12:16:05 -0400

    Link required files

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
-#main.rb
-require 'rubocop'
-require_relative 'lib/game'
+# main.rb
+require "rubocop"
+require_relative "lib/game"
+require_relative "lib/dictionary"
+require_relative "lib/display"
 
 Game.new.play
