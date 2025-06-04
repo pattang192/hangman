96b2cd7d16adf8217684b25c87214a31485a8b97
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-06-04 10:30:44 -0400

    Receive and validate player's guess

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,48 @@
 # game.rb
 require_relative 'dictionary'
+require_relative 'display'
 
 class Game
   include Dictionary
   include Display
   def initialize
-    @secret_word = DICTIONARY.sample.split.join(' ')
-    @display = '_ ' * @secret_word.length.trim
+    @word = WORD.split('').join(' ')
+    @redacted = REDACTED.split('').join(' ')
     @wrong_guesses = []
+    @all_guesses = []
   end
 
   def play
     intro
+    loop do
     input_guess
-    check_guess
-    update_display
+    update_display(@redacted, @wrong_guesses)
     conclusion
+    end
   end
 
   def input_guess
+    choose_letter
+    letter = gets.chomp.upcase
+    validate_input(letter)
   end
 
-  def check_guess
+  def validate_input(letter)
+    if letter.between?('A', 'Z') && !@all_guesses.include?(letter)
+    check_guess(letter)
+    else
+      invalid_input
+      input_guess
+    end
   end
-  
-  def update_display
+
+  def check_guess(letter)
+    if @word.include?(letter)
+     correct = (0...@word.length).find_all { |i| @word[i] == "#{letter}" }
+     correct.each { |i| @redacted[i] = letter }
+    else
+      @wrong_guesses << letter
+    end
   end
 
   def conclusion
