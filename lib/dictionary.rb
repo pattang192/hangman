7971e61458985e193b849576f10b8ccfd1678a0c
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-06-04 12:16:46 -0400

    Move word selection to game class

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,6 +2,6 @@
 
 # holds game's word set and core methods
 module Dictionary
-full_dictionary = File.open('dictionary.txt', 'r').read.split()
-DICTIONARY = full_dictionary.select{ |word| word.length.between?(5, 12)}
+  full_dictionary = File.read("./lib/dictionary.txt").split
+  DICTIONARY = full_dictionary.select { |word| word.length.between?(5, 12) }
 end
